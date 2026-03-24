904f608d1439038e90b707aa3360da9a38c8ec8d
nathanielSerrano <144270767+nathanielSerrano@users.noreply.github.com>
2026-03-24 03:33:30 -0400

    Added module descriptions
    Documentation describing the purpose of select modules added.

---
+++ apps/backend/schemas/payload.py
@@ -0,0 +1,4 @@
+"""
+This module defines the JSON schema for the input payload that the API will receive when a user uploads an image for prediction. 
+It uses Pydantic to enforce data validation and structure.
+"""
